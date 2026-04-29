c4aaf97d442f08080d9573244247978b5d62b703
editsLinux <editslinux@editsLinux.flicker-stonecat.ts.net>
2026-04-29 21:51:38 +0530

    changes made to main server file to incooperate websocket server

--- signalingServer/src/server.ts
+++ signalingServer/src/server.ts
@@ -1,18 +1,19 @@
-import express from "express";
+import { app, server } from "./httpServer.js"
+import "./websockets/ws.js"
+import { json, urlencoded } from "express"
 import { ENV } from "./lib/projectManagement/env.js";
 import { createRoomRoute } from "./routes/roomRoutes/createRoomRoute.js";
 import { joinRoomRoute } from "./routes/roomRoutes/joinRoomRoute.js";
 import { migrate } from "@db/migration.js";
 
-import { wss } from "@websocket/ws";
 
-const app = express();
 
-app.use(express.json());
+
+app.use(json());
 
 migrate();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json({ success: true, greeting: "server is running." });
@@ -23,6 +24,7 @@ app.use("/create", createRoomRoute);
 // endpoint helps to receive key and helps to join with room
 app.use("/join", joinRoomRoute);
 
-app.listen(ENV.PORT, () => {
+server.listen(ENV.PORT, () => {
   console.log(`Server is running on ${ENV.PORT}`);
 });
+
--- signalingServer/src/websocketServer/ws.ts
+++
@@ -1,8 +0,0 @@
-import { WebSocketServer } from "ws";
-export const wss = new WebSocketServer({ port: 9011 });
-console.log(wss);
-wss.on("connection", (socket) => {
-  //               ^^^^^^ what is the type of this?\\
-  //
-  console.log(typeof socket);
-});
